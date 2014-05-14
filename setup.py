c5052c70fe85bda29e6e2fe692f063cc6ddf4508
Daniel Garnier-Moiroux <git@garnier.wf>
2014-05-14 20:10:01 +0200

    Detailed setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,21 @@ from setuptools import setup
 
 setup(
     name="sqlitecookiejar",
+    description='Python FileCookieJar using SQLite files for persistence',
     py_modules=['sqlitecookiejar'],
-    version='1.0',
+    version='0.1',
     long_description=__doc__,
     zip_safe=False,
+    author_email='dgarnier@timsoft.com',
+    url='http://www.timsoft.com/',
+    license='Apache 2.0',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Session'
+    )
 )
