38c289cda624ae7d1f2bfbc6720de9b493550099
Daniel Garnier <dgarnier@timsoft.com>
2014-11-28 09:46:53 +0100

    + Added a logger, and try-except around DB calls (prevents crash from locking)

--- sqlitecookiejar.py
+++ sqlitecookiejar.py
@@ -14,6 +14,7 @@ from cookielib import FileCookieJar, Cookie
 import os.path
 import sqlite3
 import time
+import logging
 
 
 class SQLiteCookieJar(FileCookieJar):
@@ -31,17 +32,30 @@ class SQLiteCookieJar(FileCookieJar):
     -   max-age
     -   http-only
 
-    :param str filename: The complete path to the CookieJar.
-                     Defaults to HOME_FOLDER/python-cookies.sqlite
+    :param str filename:    The complete path to the CookieJar.
+                            Defaults to HOME_FOLDER/python-cookies.sqlite
+    :param logger:          The logger used to log errors, infos, etc.
+                            Defaults to a basic ERROR stdout logger.
+    :type logger:           `logging.Logger`
+
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, logger=None, *args, **kwargs):
         FileCookieJar.__init__(self, *args, **kwargs)
+
+        if logger is None:
+            self.logger = logging.getLogger(__name__)
+            handler = logging.StreamHandler()
+            handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+            handler.setLevel(logging.WARNING)
+            self.logger.addHandler(handler)
+        else:
+            self.logger = logger
+
 
         if self.filename is None:
             self.filename = os.path.join(os.path.expanduser("~"), "python-cookies.sqlite")
-            print "INFO : no filename argument specified. Your cookie jar will be in :"
-            print "   ", self.filename
+            self.logger.warning("No filename argument specified. Your cookie jar will be in %s", self.filename)
 
         self._check_and_create_table()
 
@@ -67,8 +81,10 @@ class SQLiteCookieJar(FileCookieJar):
         Used before and after loading cookies from/to the database.
         """
         with sqlite3.connect(self.filename) as con:
-            con.execute("DELETE FROM cookie WHERE expiry < ?", (time.time(),))
-
+            try:
+                con.execute("DELETE FROM cookie WHERE expiry < ?", (time.time(),))
+            except sqlite3.DatabaseError:
+                self.logger.error("Could not flush expired cookies", exc_info=True)
 
 
     def _save_cookie(self, cookie):
@@ -92,35 +108,48 @@ class SQLiteCookieJar(FileCookieJar):
             return
 
         with sqlite3.connect(self.filename) as con:
-            # print "Saving cookie [domain:", cookie.domain, ", name :", cookie.name,  ", value:", cookie.value,"]"
-            res = con.execute("SELECT id FROM cookie WHERE domain = ? AND name = ? AND path = ?",
-                (cookie.domain, cookie.name, cookie.path)).fetchone()
 
-            if res is not None:
-                id = res[0]
-                con.execute("UPDATE cookie SET value = ?, secure = ?, expiry = ?, last_access = ? WHERE id=?",
-                    (cookie.value, cookie.secure, cookie.expires, _now, id))
+            self.logger.info(
+                "SAVING cookie [domain: %s , name : %s, value: %s]" % (cookie.domain, cookie.name, cookie.value)
+            )
 
-            else:
+            try:
+                res = con.execute("SELECT id FROM cookie WHERE domain = ? AND name = ? AND path = ?",
+                    (cookie.domain, cookie.name, cookie.path)).fetchone()
 
-                if cookie.path is None or cookie.path == "":
-                    cookie.path = "/"
+                if res is not None:
+                    id = res[0]
+                    con.execute("UPDATE cookie SET value = ?, secure = ?, expiry = ?, last_access = ? WHERE id=?",
+                        (cookie.value, cookie.secure, cookie.expires, _now, id))
 
-                con.execute(
-                    "INSERT INTO "
-                        "cookie (domain,name,value, path, expiry, last_access, creation_time, secure) "
-                        "VALUES (?,?,?,?,?,?,?,?)",
-                    (
-                        cookie.domain,
-                        cookie.name,
-                        cookie.value,
-                        cookie.path,
-                        cookie.expires,
-                        _now,
-                        _now,
-                        cookie.secure
+                else:
+
+                    if cookie.path is None or cookie.path == "":
+                        cookie.path = "/"
+
+                    con.execute(
+                        "INSERT INTO "
+                            "cookie (domain,name,value, path, expiry, last_access, creation_time, secure) "
+                            "VALUES (?,?,?,?,?,?,?,?)",
+                        (
+                            cookie.domain,
+                            cookie.name,
+                            cookie.value,
+                            cookie.path,
+                            cookie.expires,
+                            _now,
+                            _now,
+                            cookie.secure
+                        )
                     )
+            except sqlite3.DatabaseError:
+                self.logger.error(
+                    "Could not save cookie [domain: %s , name : %s, value: %s]" % (cookie.domain,
+                                                                                   cookie.name,
+                                                                                   cookie.value),
+                    exc_info=True
                 )
+
 
     def load(self, filename=None, ignore_discard=False, ignore_expires=False):
         """
@@ -162,7 +191,10 @@ class SQLiteCookieJar(FileCookieJar):
                         None,
                         {}
                        )
-            # print "loaded cookie [domain: ", c.domain, ", name:", c.name, ", value:", c.value, "]"
+
+            self.logger.info(
+                "LOADED cookie [domain: %s , name : %s, value: %s]" % (c.domain, c.name, c.value)
+            )
 
             if not c.is_expired(time.time()):
                 self.set_cookie(c)
@@ -213,42 +245,50 @@ class SQLiteCookieJar(FileCookieJar):
         raise a DatabaseError.
         """
         with sqlite3.connect(self.filename) as con:
-            res = con.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
+            try:
+                res = con.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
 
-            if len(res) == 0:
-                # Case #1
-                con.execute("CREATE TABLE IF NOT EXISTS cookie "
-                            "(id INTEGER NOT NULL PRIMARY KEY,"
-                            "domain TEXT NOT NULL,"
-                            "name TEXT NOT NULL,"
-                            "value TEXT,"
-                            "path TEXT NOT NULL DEFAULT '/',"
-                            "expiry INTEGER NOT NULL,"
-                            "last_access INTEGER NOT NULL,"
-                            "creation_time INTEGER NOT NULL,"
-                            "secure INTEGER NOT NULL DEFAULT 0,"
-                            "CONSTRAINT cookie_unique UNIQUE (name, domain, path))")
-            elif len(res) == 1:
-                # Cases #2 and #3
-                table_name = res[0][0]
+                if len(res) == 0:
+                    # Case #1
+                    con.execute("CREATE TABLE IF NOT EXISTS cookie "
+                                "(id INTEGER NOT NULL PRIMARY KEY,"
+                                "domain TEXT NOT NULL,"
+                                "name TEXT NOT NULL,"
+                                "value TEXT,"
+                                "path TEXT NOT NULL DEFAULT '/',"
+                                "expiry INTEGER NOT NULL,"
+                                "last_access INTEGER NOT NULL,"
+                                "creation_time INTEGER NOT NULL,"
+                                "secure INTEGER NOT NULL DEFAULT 0,"
+                                "CONSTRAINT cookie_unique UNIQUE (name, domain, path))")
+                elif len(res) == 1:
+                    # Cases #2 and #3
+                    table_name = res[0][0]
 
-                if table_name != "cookie":
-                    # Case #3
-                    raise AttributeError("The specified database has one table named '%s', but the tabled should be named"
-                                         " 'cookie'. Please provide a valid database." % table_name)
+                    if table_name != "cookie":
+                        # Case #3
+                        raise AttributeError("The specified database has one table named '%s', but the tabled should be"
+                                             " named 'cookie'. Please provide a valid database." % table_name)
+
+                    else:
+                        # Case #2
+                        res = con.execute("PRAGMA table_info(cookie)").fetchall()
+                        columns = set([element[1] for element in res])
+                        expected_columns = {u'id', u'domain', u'name', u'value', u'path', u'expiry', u'last_access',
+                                            u'creation_time', u'secure'}
+                        if len(columns.difference(expected_columns)) > 0:
+                            raise AttributeError("The specified database has a table named 'cookie', but the column "
+                                                 "names do not match. Expected : %s with %s elements; got %s with %s "
+                                                 "elements instead." %
+                                                 (list(expected_columns),
+                                                  len(expected_columns),
+                                                  list(columns),
+                                                  len(columns)))
 
                 else:
-                    # Case #2
-                    res = con.execute("PRAGMA table_info(cookie)").fetchall()
-                    columns = set([element[1] for element in res])
-                    expected_columns = {u'id', u'domain', u'name', u'value', u'path', u'expiry', u'last_access',
-                                        u'creation_time', u'secure'}
-                    if len(columns.difference(expected_columns)) > 0:
-                        raise AttributeError("The specified database has a table named 'cookie', but the column names"
-                                             "do not match. Expected : %s with %s elements; got %s with %s elements "
-                                             "instead." %
-                                             (list(expected_columns), len(expected_columns), list(columns), len(columns)))
+                    # Case #4
+                    raise AttributeError("The specified database has more than one table. "
+                                         "Please provide a valid database.")
 
-            else:
-                # Case #4
-                raise AttributeError("The specified database has more than one table. Please provide a valid database.")
+            except sqlite3.DatabaseError:
+                self.logger.error("Sanity checks failed : could not access database", exc_info=True)
